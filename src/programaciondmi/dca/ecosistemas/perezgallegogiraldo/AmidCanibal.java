25714b8227d3a0dfd798bee7c79cdc5c4207e993
Juan Pablo Gallego <jpgarias09@gmail.com>
2016-11-09 01:54:08 -0500

    Se agregaron las clases de los personajes con cada uno de sus respectivas interfaces, aún faltan las plantas

---
+++ src/programaciondmi/dca/ecosistemas/perezgallegogiraldo/AmidCanibal.java
@@ -0,0 +1,46 @@
+package programaciondmi.dca.ecosistemas.perezgallegogiraldo;
+
+import programaciondmi.dca.core.EcosistemaAbstracto;
+import programaciondmi.dca.core.EspecieAbstracta;
+import programaciondmi.dca.core.interfaces.ICanibal;
+
+public class AmidCanibal extends EspecieAbstracta implements ICanibal{
+
+
+
+	public AmidCanibal(EcosistemaAbstracto ecosistema) {
+		super(ecosistema);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void comer(EspecieAbstracta victima) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dibujar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mover() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean recibirDano(EspecieAbstracta lastimador) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
